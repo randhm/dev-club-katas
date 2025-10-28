d7d237c59ec0fd66a3f8d2cb0bea0ba4d103bc2b
Mark Randhawa <mark.randhawa@gmail.com>
2025-10-28 16:15:17 +0000

    adding logic and tests for help-the-bookseller

--- 6kyu/help-the-bookseller/main.ts
+++ 6kyu/help-the-bookseller/main.ts
@@ -1,3 +1,25 @@
-export const stockList = (books, categories) => {
-    return '';
+export const stockList = (books: string[], categories: string[]): string => {
+    const categoryCounts: Record<string, number> = {};
+
+    const formatResults = (categoryCounts: Record<string, number>): string => {
+        const results = categories.map(category => {
+            const count = categoryCounts[category] || 0;
+            return `(${category} : ${count})`; 
+        });
+        return results.join(' - ');
+    }
+    
+    if (books.length === 0 || categories.length === 0) {
+        return formatResults(categoryCounts);
+    };
+
+    for (const book of books) {
+        const category = book[0];
+        const quantity = parseInt(book.split(' ')[1]);
+
+        categoryCounts[category] = (categoryCounts[category] || 0) + quantity;
+    }
+
+    return formatResults(categoryCounts);
 };
+
